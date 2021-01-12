1236bfb0b9db63b91411d29fc958b6b57e8e3dc4
Boeun05 <71836751+Boeun05@users.noreply.github.com>
2021-01-12 22:19:20 +0900

    p

--- frontend/src/components/Register/SchoolSearchResult.js
+++ frontend/src/components/Register/SchoolSearchResult.js
@@ -1,48 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledLabel = styled.label`
-  font-size: 12px;
-  color: #777;
-  padding-left: 5px;
-`;
-const StyledSelect = styled.select`
-  /* 공통 스타일 */
-  width: 100%;
-  outline: none;
-  border: 1px solid #ededed;
-  border-radius: 12px;
-  box-sizing: border-box;
-  color: #333;
-  padding-left: 0.5rem;
-  padding-right: 0.5rem;
-
-  /* 색상 */
-  background: #f9f9f9;
-  cd &:focus {
-    background: #fff;
-  }
-
-  /* 크기 */
-  height: 40px;
-  margin-top: 5px;
-`;
-
-function RegisterSelect({ labelName, handleOption, option, dataArr }) {
+const SchoolSearchResult = function ({ datas, handleSearchClick }) {
   return (
-    <StyledLabel>
-      {labelName}
-      <StyledSelect onChange={handleOption} value={option}>
-        {dataArr.map((data, index) => {
-          return (
-            <option value={data} key={index}>
-              {data}
-            </option>
-          );
-        })}
-      </StyledSelect>
-    </StyledLabel>
+    <ul>
+      {datas.map((data, index) => {
+        return (
+          <li onClick={handleSearchClick} key={index}>
+            {data}
+          </li>
+        );
+      })}
+    </ul>
   );
-}
+};
 
-export default RegisterSelect;
+export default SchoolSearchResult;
