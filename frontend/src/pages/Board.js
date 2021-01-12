cb46cac58fc5beb1423965d1300179d8883e3d83
gparkkii <jyp933@gmail.com>
2021-01-12 21:37:31 +0900

    board setup

--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import UserProfile from '../components/Board/UserProfile';
 import BoardInput from '../components/Board/BoardInput';
 import BoardTextarea from '../components/Board/BoardTextarea';
-import BoardArticle from '../components/Board/BoardArticle';
 import logo from '../components/image/logo.jpg';
 import profile from '../components/image/profile.png';
 import writeIcon from '../components/image/write.png';
@@ -79,6 +78,60 @@ const InputIcon = styled.img`
     height: 38px;
 `
 
+const BoardBox = styled.div`
+  background-color: #fff;
+  margin: 0px 10px;
+  padding: 15px;
+  border: 1px solid #ddd;
+
+  color: #353535;
+  font-size: 13px;
+`
+
+const BoardUser = styled.div`
+  display: flex;
+  height: 20px;
+  margin-bottom: 10px;
+`
+
+const BoardUserImg = styled.img`
+  width: 20px;
+  height: 20px;
+  border-radius: 3px;
+  margin-right: 4px;
+`
+const BoardUserID = styled.p`
+  color: #353535;
+  font-size: 11px;
+  font-weight: bold;
+  line-height: 20px;
+`
+
+const BoardTitle = styled.div`
+  font-weight: bold;
+  margin-bottom: 4px;
+`
+
+const BoardContent = styled.div`
+  font-weight: normal;
+  margin-bottom: 20px;
+`
+
+const Buttons = styled.div`
+  width: 16px;
+  height: 16px;
+`
+
+const Like = styled.button`
+  width: 16px;
+  height: 16px;
+`
+
+const Comment = styled.button`
+  width: 16px;
+  height: 16px;
+`
+
 function Board() {
 
   const [inputs, setInput] = useState({
@@ -118,7 +171,7 @@ function Board() {
         <Link to="/MyPage">
           <Profilebtn>내정보</Profilebtn>
         </Link>
-        <Link to="/Login">
+        <Link to="/">
           <Profilebtn>로그아웃</Profilebtn>
         </Link>
       </Profilebox>
@@ -139,10 +192,18 @@ function Board() {
             <InputIcon src={writeIcon}/>
         </InputButton>
       </BoardForm>
-      <BoardArticle
-        title={boardTitle}
-        content={boardContent}
-      />
+      <BoardBox>
+            <BoardUser>
+                <BoardUserImg src={profile} alt="profile"/>
+                <BoardUserID>익명</BoardUserID>
+            </BoardUser>
+            <BoardTitle>{boardTitle}</BoardTitle>
+            <BoardContent>{boardContent}</BoardContent>
+            <Buttons>
+                <Like></Like>
+                <Comment></Comment>
+            </Buttons>
+      </BoardBox>
     </Container>
   );
 }
