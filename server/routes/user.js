f55e06c2794f0505efdb36ec556e70595c0b872a
gparkkii <jyp933@gmail.com>
2021-02-22 00:31:37 +0900

    Update withdrawal delete

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const { auth } = require("../middleware/auth");
 const { User } = require("../models/User");
 const { Board } = require("../models/Board");
-const { auth } = require("../middleware/auth");
-const bcrypt = require('bcrypt');
+const { Comment } = require("../models/Comment");
+const { Like } = require("../models/Like");
 
 //=================================
 //             User
@@ -94,11 +96,24 @@ router.post('/withdrawal', auth, (req, res) => {
     User.findOne({ _id: req.body._id }, (err, user) => {
         user.comparePassword(req.body.password , (err, isMatch ) => {
             if (!isMatch) return res.json({ changeSuccess: false, message: "비밀번호가 틀렸습니다." })
-            else User.deleteOne({_id: req.body._id}, (err, user) => {
-                console.log(user);
-                if(err) return res.status(404).send();
-                else return res.json({ changeSuccess: true });
-            });
+            else {
+              Comment.deleteMany({userFrom: user._id})
+                .exec((err, result) => {
+                  return {success : true, result}
+              })
+              Like.deleteMany({userFrom: user._id})
+                  .exec((err, result) => {
+                      return {success : true, result}
+              })
+              Board.deleteMany({userFrom: user._id})
+                .exec((err, result) => {
+                  return {success : true, result}
+              })
+              User.deleteOne({_id: req.body._id}, (err, user) => {
+                  if(err) return res.status(404).send();
+                  else return res.json({ changeSuccess: true });
+              });
+            } 
         })
     })
 })
